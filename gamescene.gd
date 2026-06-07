e89143943a548328aa6927d1417d099484168109
wheatgreaser <wheatgreaser3000@gmail.com>
2026-06-07 15:44:45 +0530

    feat: portalling

--- gamescene.gd
+++ gamescene.gd
@@ -7,6 +7,9 @@ var rng = RandomNumberGenerator.new()
 
 var room = preload("res://room.tscn")
 var door = preload("res://door.tscn")
+
+
+
 func mapper(grid):
 	var k = 1
 	for i in range(3):
@@ -40,7 +43,7 @@ func pathgen(pos, path, visited, map):
 	
 func _ready():
 	rng.randomize()
-
+	var door_pos = []
 	mapper(map1)
 	mapper(map2)
 	pathgen(Vector2(0,0), [], {}, map1)
@@ -55,30 +58,38 @@ func _ready():
 		for j in range(3):
 			if (map1[i][j] == path1[k1] and k1 < path1.size()):
 				var room_instance = room.instantiate()
-				var door_instance = door.instantiate()
-				room_instance.position = Vector3(i, 0, j)
-				door_instance.position = Vector3(i, 0, j)
+				
+				room_instance.position = Vector3(i* 5, 0, j*5)
+				room_instance.scale = Vector3(5,5,5)
 				add_child(room_instance)
-				add_child(door_instance)
+				
 				map1[i][j] = 1
+				door_pos.append(Vector3(i,0,j))
 				k1 += 1
 			else:
 				map1[i][j] = 0
-			
-	print(path1)
-	print(map1)
-	
+				
+	for dpos in range(door_pos.size()-1):
+		var door_instance = door.instantiate()
+		door_instance.position = Vector3(((door_pos[dpos].x + door_pos[dpos+1].x)/2)*5, 0, ((door_pos[dpos].z+ door_pos[dpos+1].z)/2)*5)
+		door_instance.scale = Vector3(5,5,5)
+		add_child(door_instance)
+		
+						
 	for i in range(3):
 		for j in range(3):
-			if (map2[i][j] == path2[k2] and k2 < path2.size()):
+			if (map2[i][j] == path2[k2] and k2 < path1.size()):
+				var room_instance = room.instantiate()
+				
+				room_instance.position = Vector3(i* 5, 20, j*5)
+				room_instance.scale = Vector3(5,5,5)
+				add_child(room_instance)
+				
 				map2[i][j] = 1
+				door_pos.append(Vector3(i,20,j))
 				k2 += 1
 			else:
 				map2[i][j] = 0
-			
-	print(path2)
-	print(map2)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+
+
